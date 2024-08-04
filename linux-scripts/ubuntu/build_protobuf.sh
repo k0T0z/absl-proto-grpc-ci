4fa5bee858ef0f51089b0efe59628a501f05e6f6
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2024-08-04 19:15:47 +0300

    Create build_protobuf.sh

---
+++ linux-scripts/ubuntu/build_protobuf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create custom directory
+mkdir -p $GITHUB_WORKSPACE/proto-k0t0z-lib
+
+# Clone Abseil
+git clone --depth 1 -b v27.3 https://github.com/protocolbuffers/protobuf.git
+
+cd protobuf
+
+# Checkout the latest release
+git submodule update --init --recursive
+
+# Create build directory
+mkdir build && cd build
+
+# Configure and build
+cmake .. -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_ABSL_PROVIDER=package -DCMAKE_PREFIX_PATH=$GITHUB_WORKSPACE/absl-k0t0z-lib -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/proto-k0t0z-lib
+make -j$(nproc)
+
+# Install
+make install
+
+# Go back to the original directory
+cd $GITHUB_WORKSPACE
