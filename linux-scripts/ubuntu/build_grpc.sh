1e21537c3d530eba069f2fcbd7d4be3b25ae1444
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2024-08-04 19:20:57 +0300

    Create build_grpc.sh

---
+++ linux-scripts/ubuntu/build_grpc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Create custom directory
+mkdir -p $GITHUB_WORKSPACE/grpc-k0t0z-lib
+
+# Clone Abseil
+git clone --depth 1 -b v1.65.4 https://github.com/grpc/grpc
+
+cd grpc
+
+# Checkout the latest release
+git submodule update --init --recursive
+
+# Create build directory
+mkdir build && cd build
+
+# Configure and build
+cmake .. -DgRPC_INSTALL=ON \
+         -DCMAKE_BUILD_TYPE=Release 
+         -DgRPC_ABSL_PROVIDER=package 
+         -DgRPC_CARES_PROVIDER=package 
+         -DgRPC_PROTOBUF_PROVIDER=package 
+         -DgRPC_RE2_PROVIDER=package 
+         -DgRPC_SSL_PROVIDER=package 
+         -DgRPC_ZLIB_PROVIDER=package 
+         -DCMAKE_PREFIX_PATH="$GITHUB_WORKSPACE/absl-k0t0z-lib;$GITHUB_WORKSPACE/proto-k0t0z-lib"
+         -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/grpc-k0t0z-lib
+
+make -j$(nproc)
+
+# Install
+make install
+
+# Go back to the original directory
+cd $GITHUB_WORKSPACE
