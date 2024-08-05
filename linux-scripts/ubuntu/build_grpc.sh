99c47c8f56ced9890c623382267d063adb424424
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2024-08-05 22:07:48 +0300

    Update build_grpc.sh

--- linux-scripts/ubuntu/build_grpc.sh
+++ linux-scripts/ubuntu/build_grpc.sh
@@ -21,11 +21,11 @@ mkdir build && cd build
 cmake .. -DgRPC_INSTALL=ON \
          -DCMAKE_BUILD_TYPE=Release \
          -DgRPC_ABSL_PROVIDER=package \
-         -DgRPC_CARES_PROVIDER=package \
+         -DgRPC_CARES_PROVIDER=module \
          -DgRPC_PROTOBUF_PROVIDER=package \
-         -DgRPC_RE2_PROVIDER=package \
-         -DgRPC_SSL_PROVIDER=package \
-         -DgRPC_ZLIB_PROVIDER=package \
+         -DgRPC_RE2_PROVIDER=module \
+         -DgRPC_SSL_PROVIDER=module \
+         -DgRPC_ZLIB_PROVIDER=module \
          -DCMAKE_PREFIX_PATH="$GITHUB_WORKSPACE/absl-k0t0z-lib;$GITHUB_WORKSPACE/proto-k0t0z-lib" \
          -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/grpc-k0t0z-lib
 
